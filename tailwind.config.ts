35d6ed74555ba3c73b05321aad4e00c30b8ed343
Henry Bugajski <henry.bug.dev@gmail.com>
2023-08-23 13:33:27 -0400

    feat: create base site

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,26 @@
 import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    fontFamily: {
+      sans: [
+        ['var(--font-archivo)', 'sans-serif', ...defaultTheme.fontFamily.sans],
+        {
+          fontFeatureSettings: "'liga'",
+          fontVariationSettings: "'wght' 375",
+        },
+      ],
+      serif: [
+        ['var(--font-clash-display)', 'serif', ...defaultTheme.fontFamily.serif],
+        {
+          fontFeatureSettings: "'liga'",
+          fontVariationSettings: "'wght' 450",
+        },
+      ],
     },
   },
-  plugins: [],
 };
+
 export default config;
