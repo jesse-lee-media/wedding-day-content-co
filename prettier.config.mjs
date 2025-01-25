d67bd4da3fe03f6d7fe76dd6769d130b29e4f85e
Henry Bugajski <henry.bug.dev@gmail.com>
2025-01-25 14:15:03 -0500

    chore: update prettier

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import("prettier").Config} */
 export default {
   plugins: ['prettier-plugin-tailwindcss'],
-  bracketSpacing: true,
   printWidth: 100,
-  semi: true,
   singleQuote: true,
-  tabWidth: 2,
-  trailingComma: 'all',
-  useTabs: false,
 };
