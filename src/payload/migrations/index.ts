2c5b5346ef437df5a8b0dcd013d2420d88d975ce
Henry Bugajski <henry.bug.dev@gmail.com>
2025-02-24 15:20:16 -0500

    chore: generate migrations

---
+++ src/payload/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250224_201938 from './20250224_201938';
+
+export const migrations = [
+  {
+    up: migration_20250224_201938.up,
+    down: migration_20250224_201938.down,
+    name: '20250224_201938'
+  },
+];
